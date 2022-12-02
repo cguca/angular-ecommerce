8e29b684ed1ea7e92de781e96135ee87a16ddd41
Cary Guca <cary.guca@gmail.com>
2022-12-01 20:19:31 -0600

    Checkout page

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup!: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+              private luv2ShopFormService: Luv2ShopFormService) { }
+
+  ngOnInit(): void {
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log(`The start month ${startMonth}`);
+        console.log(`The month array ${JSON.stringify(data)}`);
+        this.creditCardMonths = data;
+      }
+    );
+
+    // populate credit card years
+    this.luv2ShopFormService.getCreditCardYears().subscribe(
+        data => {
+          this.creditCardYears = data;
+        }
+    );
+
+  }
+
+  copyShippingAddressToBillingAddress(event: Event) {
+
+    if((<HTMLInputElement>event.target).checked) {
+      this.checkoutFormGroup.controls['billingAddress'].setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    }
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
+  onSubmit() {
+    console.log("Handling submission");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  
+}

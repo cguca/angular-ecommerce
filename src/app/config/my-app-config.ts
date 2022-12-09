d5fa4ec7dc89b4e0eef8bb875c7e4b7a2523a4fb
Cary Guca <cary.guca@gmail.com>
2022-12-09 07:42:31 -0600

    secure communications

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa7ijypmqK7DmcxF5d7',
         issuer: 'https://dev-30398678.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
